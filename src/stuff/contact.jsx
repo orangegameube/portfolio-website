47b7e92c84ed4b771ddda18b27f1e2cb1b094c63
orangegameube <156160264+orangegameube@users.noreply.github.com>
2025-08-26 15:17:04 +0000

    Got a working contact form using react, css, and web3forms! Using css style to resize flex container to properly be filled. Project is almost finished, just need to mess with and style navbar!

--- src/stuff/contact.jsx
+++ src/stuff/contact.jsx
@@ -1,47 +1,54 @@
+import './contact.css'
+
+const Contact = () => {
 
 
-function Contact() {
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "731875b0-b687-4a7c-9502-65a92d5021f5");
+
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: json
+        }).then((res) => res.json());
+
+        if (res.success) {
+        console.log("Success", res);
+        }
+    };
 
 
-    const skhead = {
-        marginTop: "8%",
-        color: "DodgerBlue",
-        fontSize: "40px",
-        fontWeight: "bold",
-    }
 
-    const sksub = {
-        fontSize: "20px",
-        padding: "15px"
-    }
-
-    const expcard = {
-        backgroundColor: "white",
-        borderRadius: "8px",
-        boxShadow: "5px 10px 18px #888888",
-        margin: "2.5% 7% 2.5% 7%",
-        padding: "30px 40px 10px 40px",
-        textAlign: "left"
-    }
-
-
-    return (
-        <div>
-            <h2 style={skhead}>Contact</h2>
-            <p style={sksub}>Feel free to send a message through the contact from below or connect with me on social media!</p>
-            <div>
-                
-
-
-                <div style={expcard}>
-                    <h4>Linkedin</h4>
-                    <h4>GitHub</h4>
+    return(
+        <section className="contactbox">
+            <form onSubmit={onSubmit}>
+                <h2 className="conthead">Contact Me</h2>
+                <div className="inputbox">
+                    <label className="labellab">Name</label>
+                    <input type="text" className="field" placeholder="Enter your name" name="name" required />
                 </div>
+                <div className="inputbox">
+                    <label className="labellab">Email Address</label>
+                    <input type="email" className="field" placeholder="Enter your email" name="email" required />
+                </div>
+                <div className="inputbox">
+                    <label className="labellab">Your Message</label>
+                    <textarea name="message" className="field mess" placeholder="Enter your message" required></textarea>
+                </div>
+                <button type="submit">Send Message</button>
+            </form>
+        </section>
+    )
+  
+};
 
-            </div>
-        </div>
-    );
-
-}
-
-export default Contact
+export default Contact;
