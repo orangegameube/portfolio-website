3f412d92de071ca507cedc00934a509f97c137c4
orangegameube <156160264+orangegameube@users.noreply.github.com>
2025-08-27 22:00:03 +0000

    Deleted original emailjs directory due to switching to web3forms. fixed potential errors for deploy in package.json and vite config.

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  root: 'src',
-  base: '/portfolio-website/',
   publicDir: '../public',
   resolve: {
     alias: {
